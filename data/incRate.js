b853955e8c0ff06d7d305c80c769099a476f0181
min <mmbliv@gmail.com>
2023-03-14 21:22:35 -0500

    generate incRateData for each cancer type

--- data/incRate.js
+++ data/incRate.js
@@ -1,9 +1,15 @@
 const convertData = require("../utils/convertData.js");
 const generateDocBasedOnsex = require("../utils/generateDataBasedOnSex.js");
+const generateDataBasedOnCancerType = require("../utils/generateDataBasedOnCancerType.js");
 const path = require("path");
 const dataPath = path.join(__dirname, "IncRate.xlsx");
 const incRateData = convertData(7, "sheet1", dataPath);
+const incRateData2 = convertData(8, "sheet1", dataPath);
 
-const incRate = generateDocBasedOnsex(incRateData, "incidence_rates_2015_2019");
-// console.log(newCase);
-module.exports = incRate;
+const incRate1 = generateDocBasedOnsex(
+  incRateData,
+  "incidence_rates_2015_2019"
+);
+const incRate2 = generateDataBasedOnCancerType(incRateData2["All US"]);
+// console.log(incRate2);
+module.exports = { incRate1, incRate2 };
--- db/seed.js
+++ db/seed.js
@@ -15,7 +15,7 @@ const data = [
     category: allCategory.category,
     new_case_2023: newCase.new_case_2023,
     death_rates_2016_2020: death.death1.death_rates_2016_2020,
-    incidence_rates_2015_2019: incRate.incidence_rates_2015_2019,
+    incidence_rates_2015_2019: incRate.incRate1.incidence_rates_2015_2019,
     death_rates_2016_2020: deathRate.deathRate1.death_rates_2016_2020,
     probability_of_developing_cancer_2017_2019:
       proDev.probability_of_developing_cancer_2017_2019,
