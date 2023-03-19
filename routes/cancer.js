173bcd3cfd9af011e2a3d9e25dfa35e632ec7c13
min <mmbliv@gmail.com>
2023-03-19 09:58:28 -0500

    add authorize to all post and put method

--- routes/cancer.js
+++ routes/cancer.js
@@ -3,12 +3,12 @@ const cancerController = require("../controllers/cancer.js");
 const router = express.Router();
 const authorize = require("../utils/user/helpers.js");
 
-router.get("/cancer", authorize(["user", "admin"]), cancerController.index);
+router.get("/cancer", cancerController.index);
 // router.get("/cancer/us", cancerController.getUsData);
 // router.get("/cancer/category", cancerController.getCancerCategory);
 // router.get("/cancer/cancer_data", cancerController.getAllTypeData);
 router.get("/cancer/:cancer", cancerController.getOneTypeData);
 // router.put("/cancer/us/:item", cancerController.updateUsData);
 router.post("/cancer", authorize("admin"), cancerController.create);
-router.put("/cancer/:cancer", cancerController.updata);
+router.put("/cancer/:cancer", authorize("admin"), cancerController.updata);
 module.exports = router;
--- routes/states.js
+++ routes/states.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const stataController = require("../controllers/states.js");
 const router = express.Router();
+const authorize = require("../utils/user/helpers.js");
 
 router.get("/state", stataController.index);
 router.get("/state/:state", stataController.showStateData);
 // router.post("/state/cancer/:state-:cancer", stataController.create);
 router.get("/state/cancer/:state-:cancer", stataController.showCancerInState);
-router.put("/state/cancer/:state-:cancer", stataController.updata);
-router.post("/state/cancer/:state-:cancer", stataController.create);
+router.put(
+  "/state/cancer/:state-:cancer",
+  authorize("admin"),
+  stataController.updata
+);
+router.post(
+  "/state/cancer/:state-:cancer",
+  authorize("admin"),
+  stataController.create
+);
 
 module.exports = router;
